321342a8db175305f2e624324ff00539447fd59b
Carsten Kremser <github@carstenkremser.de>
2024-04-18 12:28:08 +0200

    Add Headline to Navigation

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,10 +1,12 @@
 import {Link} from "react-router-dom";
 
 export default function Navigation() {
-    return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/characters">Character Gallery</Link>
-        </nav>
+    return (<>
+            <h1>Rick and Morty Gallery</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/characters">Character Gallery</Link>
+            </nav>
+        </>
     )
 }
