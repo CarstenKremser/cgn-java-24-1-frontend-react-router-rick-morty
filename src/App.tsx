aeec1809782971ebd5168a67f901d12b18e81a1f
Carsten Kremser <github@carstenkremser.de>
2024-04-18 12:23:45 +0200

    Add Gallery and Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import './App.css'
 import {useState} from "react";
 import {Character, charactersResponse} from "./characters.ts";
 import CharacterGallery from "./CharacterGallery.tsx";
+import {Route, Routes} from 'react-router-dom';
+import CharacterCard from "./CharacterCard.tsx";
+import Navigation from "./Navigation.tsx";
+import Welcome from "./Welcome.tsx";
+
+//                <CharacterGallery  characters={characters}/>
 
 function App() {
 
@@ -9,7 +15,11 @@ function App() {
 
     return (
         <>
-            <CharacterGallery  characters={characters}/>
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Welcome/>}/>
+                <Route path={"/characters"} element={<CharacterGallery characters={characters}/>}/>
+            </Routes>
         </>
     )
 }
---
+++ src/Welcome.tsx
@@ -0,0 +1,6 @@
+export default function Welcome() {
+    return (<>
+        <h1>Welcome!</h1>
+        <p>Welcome to Rick and Morty Gallery</p>
+    </>)
+}
